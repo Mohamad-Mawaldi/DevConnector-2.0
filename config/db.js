2516bdbb49893a54d653351733761918e6d1eb5f
Mohamad <ima.mawaldi@gmail.com>
2019-12-18 21:26:24 +0100

    added the connection the DB

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("works fine :) we are connected with the DB");
+  } catch (error) {
+    console.log("error", error.message);
+
+    //Exit the process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
